983adc713ae2d4cde3da705ae316880a316cf349
Stiliyan Ivanov <stily.c@gmail.com>
2019-04-05 13:30:00 +0300

    Fix indents

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
-	"extends": "airbnb-base",
-	"parserOptions": {
+    "extends": "airbnb-base",
+    "parserOptions": {
 
-	},
-	"plugins": [],
-	"rules": {
-		"no-console": 0,
-		"indent": ["error", 4, {
-			"MemberExpression": 1,
-			"SwitchCase": 1
-		}],
-		"no-tabs": "off",
-		"camelcase": [2, {"properties": "always"}],
+    },
+    "plugins": [],
+    "rules": {
+        "no-console": 0,
+        "indent": ["error", 4, {
+            "MemberExpression": 1,
+            "SwitchCase": 1
+        }],
+        "no-tabs": "off",
+        "camelcase": [2, {"properties": "always"}],
 
-		"import/prefer-default-export": "off",
-		"no-use-before-define": "off",
-		"comma-dangle": ["error", {
-			"arrays": "always-multiline",
-			"objects": "always-multiline",
-			"imports": "never",
-			"exports": "never",
-			"functions": "never"
-		}]
-	},
-	"env": {
-		"browser": true,
-		"es6": true,
-	},
+        "import/prefer-default-export": "off",
+        "no-use-before-define": "off",
+        "comma-dangle": ["error", {
+            "arrays": "always-multiline",
+            "objects": "always-multiline",
+            "imports": "never",
+            "exports": "never",
+            "functions": "never"
+        }]
+    },
+    "env": {
+        "browser": true,
+        "es6": true,
+    },
     "parserOptions": {
         "ecmaVersion": 8,
         "sourceType": "script"
