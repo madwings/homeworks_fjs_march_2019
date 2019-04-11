f39d571bf201f97fdee0e99d8efe9fa80fef9f4d
Nikola Raev <48968853+nraevv@users.noreply.github.com>
2019-04-11 20:14:46 +0300

    Delete script.js

--- HOMEWORK_raevv/task3/script.js
+++
@@ -1,8 +0,0 @@
-var firstNum = parseFloat(prompt('Enter a number: ').replace(',', '.'));
-var secondNum = parseFloat(prompt('Enter a number: ').replace(',', '.'));
-if (isNaN(firstNum)) {
-	console.log('First enter is not a number!');
-}
- else  (isNaN(secondNum)) 
-    console.log('Second enter is not number!');
-
